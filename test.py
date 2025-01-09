21e95266e9365cbdb15fab97d8296256897855f6
dac <dac@dypm.org>
2025-01-09 11:38:09 +0530

    file modified

--- test.py
+++ test.py
@@ -8,3 +8,14 @@ sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Multiplication table (from 1 to 10) in Python
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
